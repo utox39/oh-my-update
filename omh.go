f1392513a51b47859b3bdf71b0bb8820c5a6cb37
utox39 <francescomoccaldi39@gmail.com>
2024-02-24 22:19:39 +0100

    first commit

---
+++ omh.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"github.com/go-git/go-git/v5"
+	"os"
+	"path"
+)
+
+func UpdateRepo(gitFolder string) error {
+	repo, err := git.PlainOpen(gitFolder)
+	if err != nil {
+		return err
+	}
+
+	worktree, err := repo.Worktree()
+	if err != nil {
+		return err
+	}
+
+	var output bytes.Buffer
+	err = worktree.Pull(&git.PullOptions{
+		RemoteName:    "origin",
+		ReferenceName: "refs/heads/master",
+		SingleBranch:  true,
+		Progress:      &output,
+	})
+
+	if output.String() != "" {
+		fmt.Println(output.String())
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func UpdateFolder(folder string) {
+	fmt.Printf("Updating %s...\n\n", folder)
+
+	homeFolder, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+
+	omzFolder := path.Join(homeFolder, ".oh-my-zsh/custom", folder)
+
+	if _, err := os.Stat(omzFolder); errors.Is(err, os.ErrNotExist) {
+		panic(err)
+	}
+
+	entries, err := os.ReadDir(omzFolder)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, e := range entries {
+		if e.IsDir() && e.Name() != "example" {
+			fmt.Println("Updating:", e.Name())
+			gitFolder := path.Join(omzFolder, e.Name())
+			if updateErr := UpdateRepo(gitFolder); updateErr != nil {
+				HandleError(updateErr, e.Name())
+			} else {
+				fmt.Println(e.Name(), "updated successfully")
+				fmt.Println()
+			}
+		}
+	}
+}
+
+func HandleError(err error, pluginName string) {
+	if errors.Is(err, git.NoErrAlreadyUpToDate) {
+		fmt.Printf("%s is %s\n\n", pluginName, err)
+	} else {
+		fmt.Printf("omh: %s: %s\n\n", pluginName, err)
+	}
+}
+
+func main() {
+	UpdateFolder("plugins")
+	UpdateFolder("themes")
+}
